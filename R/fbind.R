237e1f520bcb5db250d22a801f76644f49400967
li-jinyan <1193034560@qq.com>
2020-07-21 15:39:42 +0800

    7.21test1

---
+++ R/fbind.R
@@ -0,0 +1,15 @@
+# combine a and b
+#
+# This is an example function named 'fbind'
+# which add list a and b together
+#
+#' hello
+#'
+#' @inheritParams fbind
+#'
+#' @return combined charactors
+#' @export
+#'
+fbind <- function(a,b){
+  factor(c(as.character(a),as.character(b)))
+}
--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,6 +12,13 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+#' hello
+#'
+#' @inheritParams hello
+#'
+#' @return print hello world
+#' @export
+#'
 
 hello <- function() {
   print("Hello, world!")
